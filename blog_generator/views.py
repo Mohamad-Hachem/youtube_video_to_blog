d37424e496c596da09f4eae70ba1f7bce37600b0
Mohamad-Hachem <mohamad27hachem@outlook.com>
2024-06-10 15:47:23 +0200

    1:37hr

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -4,3 +4,20 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def user_login(request):
+    return render(request, 'login.html')
+
+
+def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repeatPassword = request.POST['repeatpassword']
+    return render(request, 'signup.html')
+
+
+def user_logout(request):
+    return render(request, 'logout.html')
